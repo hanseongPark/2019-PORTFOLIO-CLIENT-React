6e90e469f1241ceec0d88e34cc11d466065574b9
hanseongPark <hsung901231@gmail.com>
2019-09-01 19:47:03 +0900

    github add/progressbar change

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
 		<Container className="footer text-center">
 			<Row>
                 <Col md={4} xs={12}>
-                    <a className="nav-link text-muted" rel="noopener noreferrer" href="https://github.com/hanseongPark/phs-portfolio" target="_blank"><p><img className="img-circle-footer" src={git} alt="Generic placeholder image" width="30" height="30"/>Github</p></a>
+                    <a className="nav-link text-muted" rel="noopener noreferrer" href="https://github.com/hanseongPark" target="_blank"><p><img className="img-circle-footer" src={git} alt="Generic placeholder image" width="30" height="30"/>Github</p></a>
                 </Col>
                 <Col md={4} xs={12}>
                     <p className="nav-link text-muted"><img className="img-circle-footer" src={mail} alt="Generic placeholder image" width="30" height="30"/>hsung901231@google.com</p>
